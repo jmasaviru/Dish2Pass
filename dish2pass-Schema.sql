4fde53dc06fac1f4a2a9d6323e11282583b2ce4e
James Masaviru <jmasaviru@gmail.com>
2020-12-22 17:32:07 -0500

    Checking if Travis runs Linter

--- dish2pass-Schema.sql
+++ dish2pass-Schema.sql
@@ -1,13 +1,13 @@
 -- Drops the database if it exists currently --
 DROP DATABASE IF EXISTS dish2pass_db;
 
--- Creates the "dish2passDB" database --
+-- Creates the "dish2pass_db" database --
 CREATE DATABASE dish2pass_db;
 
--- Makes it so all of the following code will affect dish2passDB --
+-- Makes the following code affect dish2pass_db --
 USE dish2pass_db;
 
--- Creates the table "users" within dish2passDB --
+-- Creates the table "users" within dish2pass_db --
 CREATE TABLE users (
   id INT NOT NULL AUTO_INCREMENT,
   username VARCHAR(45) NULL,
@@ -15,7 +15,7 @@ CREATE TABLE users (
   PRIMARY KEY (id)
 );
 
--- Creates the table "recipes" within dish2passDB --
+-- Creates the table "recipes" within dish2pass_db --
 CREATE TABLE recipes (
   id INT NOT NULL AUTO_INCREMENT,
   recipes VARCHAR(45) NULL,
