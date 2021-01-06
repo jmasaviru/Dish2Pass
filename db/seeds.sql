b5a31d8f6e0729d6b3b067cff9092f4f5e1e266e
Brian Short <brian.ralph.short@gmail.com>
2021-01-06 16:25:53 -0500

    Seed file for RecipeTwo table in dish2pass_db

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE dish2pass_db;
+
+INSERT INTO RecipeTwos (recipeId, recipe)
+VALUES (137289, "Citrus-Rubbed Whole Duck"), 
+(23396, "Crispy Duck"),
+(619797, "Roasted Duck with Apples"),
+(485680, "Sweet and Sour Brisket");
