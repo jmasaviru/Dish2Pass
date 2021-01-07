129a99b204420fb4b6135490c39e0af0c00a1caf
Brian Short <brian.ralph.short@gmail.com>
2021-01-07 10:19:31 -0500

    Update simplified schema to build only database

--- db/dish2pass-Schema.sql
+++ db/dish2pass-Schema.sql
@@ -3,21 +3,3 @@ DROP DATABASE IF EXISTS dish2pass_db;
 
 -- Creates the "dish2pass_db" database --
 CREATE DATABASE dish2pass_db;
-
--- Makes the following code affect dish2pass_db --
-USE dish2pass_db;
-
--- Creates the table "users" within dish2pass_db --
-CREATE TABLE users (
-  id INT NOT NULL AUTO_INCREMENT,
-  username VARCHAR(45) NULL,
-  pswd VARCHAR(45) NULL,
-  PRIMARY KEY (id)
-);
-
--- Creates the table "recipes" within dish2pass_db --
-CREATE TABLE recipes (
-  id INT NOT NULL AUTO_INCREMENT,
-  recipes VARCHAR(45) NULL,
-  PRIMARY KEY (id)
-);
